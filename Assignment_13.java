922cfc44249e316c7a52441f0d5a322b8453323a
Pavankumar99999 <golipavankumar9999@gmail.com>
2024-02-15 11:52:39 +0530

    Assignment_13

---
+++ Assignment_13.java
@@ -0,0 +1,21 @@
+package mkt_Assignments;
+/*
+ Write a Program to  create SIB
+ */
+public class Assignment_13 //main class
+{
+
+	static //(SIB) Static Initialization Block
+	{
+		System.out.println("This is a SIB Block");
+		
+	}
+	
+	public static void main(String args[]) //main method
+	{
+		System.out.println("This is a main method");
+	}
+	
+	
+	
+}
